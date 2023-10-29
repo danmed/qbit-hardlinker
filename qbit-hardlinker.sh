3184587fb4429dbd988c593dc6acfa642bd5b7f6
danmed <danmed@gmail.com>
2023-10-29 23:13:13 +0000

    Add Category to hardlink path

--- qbit-hardlinker.sh
+++ qbit-hardlinker.sh
@@ -3,12 +3,13 @@
 # Get the path to the download and the name of the torrent
 download_path="$2"
 torrent_name="$1"
+torrent_label="$3"
 
 # The destination folder
-destination="/media/ready"
+destination="/home4/user/downloads/qbittorrent/$torrent_label"
 
 # Validate that the required parameters have been passed
-if [ -z "$download_path" ] || [ -z "$torrent_name" ] || [ -z "$destination" ]; then
+if [ -z "$download_path" ] || [ -z "$torrent_name" ] || [ -z "$destination" ] || [ -z "$torrent_label" ]; then
   echo "Error: missing required parameters."
   echo "Usage: $0 <torrent name> <download path>"
   exit 1
